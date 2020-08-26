b4cdf1f6da1812a1ef6fcb124cb1bbe7f29274ad
KhairulAlamNahid <profreelancer10@gmail.com>
2020-08-27 01:56:19 +0600

    modified

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-// service slider
+// latest new venobox
+$('.venobox').venobox();
 
+
+// service slider
 $('.service-slider').slick({
     dots: true,
     infinite: true,
@@ -41,4 +44,82 @@ $('.service-slider').slick({
 $('.counter').counterUp({
     delay: 20,
     time: 2000
+});
+
+
+//news slider js
+$('.img-main').slick({
+    dots: false,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
+    prevArrow: '<i class="fa fa-chevron-left prev" aria-hidden="true"></i>',
+    nextArrow: '<i class="fa fa-chevron-right next" aria-hidden="true"></i>',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+    },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+    },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+    }
+  ]
+});
+
+
+//customer slider js
+$('.customer-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
+    prevArrow: '<i class="fa fa-chevron-left prev" aria-hidden="true"></i>',
+    nextArrow: '<i class="fa fa-chevron-right next" aria-hidden="true"></i>',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+    },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+    },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+    }
+  ]
 });
